f32d0d8ebfa578eaf153a67cc2b71144a21c9b2a
Juan Antonio Marquez Ruiz <bymarquezz2@gmail.com>
2023-02-01 18:27:26 +0100

    home acabado

--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { Firestore, collectionData, collection, query, where } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-pie',
@@ -8,9 +7,5 @@ import { Observable } from 'rxjs';
   styleUrls: ['./pie.component.css']
 })
 export class PieComponent {
-  item$: Observable<any>;
-  constructor(firestore: Firestore) {
-    const collectionBD = collection(firestore, 'items');
-    this.item$ = collectionData(query(collectionBD, where("nombre", "==", "pepe")));
-  }
+
 }
